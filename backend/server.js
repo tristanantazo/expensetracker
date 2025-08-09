a58a66341a34545b7a65cdb2ad22a7af8fba9066
Tristan Viel Antazo <tristan.antazo@ringcentral.com>
2025-08-09 15:41:39 +0800

    first commit

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,3 @@ app.use(router);
 app.listen(port, () => {
   console.log(`Node.js bsackend running at http://localhost:${port}`);
 });
-
-
-// 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
