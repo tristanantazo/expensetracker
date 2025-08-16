1aa65729ec41237bef0d7037bd91e682711cbe39
Tristan Viel Antazo <tristan.antazo@ringcentral.com>
2025-08-16 08:17:23 +0800

    d

--- backend/app/utils/GoogleAPI.js
+++ backend/app/utils/GoogleAPI.js
@@ -7,8 +7,8 @@ console.log(process.env.GOOGLE_SERVICE_ACCOUNT_TYPE)
 const keys = {
     "type": process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
     "project_id":process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
-    "private_key_id":process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-    "private_key":process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "private_key_id":process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID.replace(/\\n/g, '\n'),
+    "private_key":process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email":process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
     "client_id":process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
     "auth_uri":process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
